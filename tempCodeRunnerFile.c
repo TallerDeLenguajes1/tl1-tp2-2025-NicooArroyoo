f72323c75e525386a4319b0bb9f75a438ce2e969
Nicolas <nicolasmarroyoz@gmail.com>
2025-04-06 00:39:57 -0300

    agregue funcion listarPcs

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,3 @@
+s =  1 + rand () % 8;
+        pcs[i].tipo_cpu = &tipos[1 +rand () % 6];
+    }
--- tp2_4.c
+++ tp2_4.c
@@ -9,23 +9,36 @@ struct compu {
  char *tipo_cpu; // Tipo de procesador (apuntador a cadena de caracteres)
 } typedef PC;
 
+void listarPCs(PC pcs[], int cantidad);
+
 int main(){
     
     srand(time(NULL));
-    PC pcs[5];
+    int cantidad = 5;
+    PC pcs[cantidad];
     int velocidad, anio, cantNucleos;
-    char tipos[]= {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
+    char *tipos[] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
     char *tipoCpu;
     
-    for(int i=0;i<5;i++){
+    for(int i=0;i<cantidad;i++){
         pcs[i].velocidad = 1 +rand () % 3;
         pcs[i].anio = 2015 + rand() % 10;
         pcs[i].cantidad_nucleos =  1 + rand () % 8;
-        pcs[i].tipo_cpu = &tipos[1 +rand () % 6];
+        pcs[i].tipo_cpu = tipos[rand ()%6];
     }
-   
 
+    listarPCs(pcs,cantidad);
 
   return 0;
+}
+
+void listarPCs(PC pcs[], int n){
+  for(int i=0;i<n;i++){
+    printf("---- PC %d ----\n",i+1);
+    printf("Velocidad: %d Ghz\n",pcs[i].velocidad);
+    printf("Anio de fabricacion: %d \n",pcs[i].anio);
+    printf("Cantidad de nucleos: %d \n",pcs[i].cantidad_nucleos);
+    printf("Tipo de procesador: %s \n\n",pcs[i].tipo_cpu);   
+  }
 }
 
